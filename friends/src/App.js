ae67422ef463fbea5cc655ae05e1fcfde96ef93b
Nicky Chen <nicky.chen0@gmail.com>
2018-12-29 09:07:49 -0800

    add new friends

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
 import { connect } from "react-redux";
-import { fetchFriends } from "./actions";
+import { fetchFriends, addFriends } from "./actions";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      age: "",
+      email: ""
+    };
+  }
   componentDidMount() {
     this.props.fetchFriends();
   }
+  inputFriends = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  addFriends = e => {
+    e.preventDefault();
+    const newFriend = {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email
+    };
+    this.setState({ name: "", age: "", email: "" });
+    this.props.addFriends(newFriend);
+  };
   render() {
     const { friend } = this.props;
     console.log(friend);
@@ -16,6 +37,22 @@ class App extends Component {
         {friend.friends.map(person => {
           return <div key={person.id}>{person.name}</div>;
         })}
+        <form>
+          <input
+            value={this.state.name}
+            onChange={this.inputFriends}
+            name="name"
+            placeholder="Name"
+          />
+          <input value={this.state.age} onChange={this.inputFriends} name="age" placeholder="Age" />
+          <input
+            value={this.state.email}
+            onChange={this.inputFriends}
+            name="email"
+            placeholder="Email"
+          />
+          <button onClick={this.addFriends}>Add Friend</button>
+        </form>
       </div>
     );
   }
@@ -29,5 +66,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  { fetchFriends }
+  { fetchFriends, addFriends }
 )(App);
--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -2,6 +2,8 @@ import axios from "axios";
 export const FETCH_FRIENDS_STARTED = "FETCH_FRIENDS_STARTED";
 export const FETCH_FRIENDS_SUCCESS = "FETCH_FRIENDS_SUCCESS";
 export const FETCH_FRIENDS_ERROR = "FETCH_FRIENDS_ERROR";
+export const ADD_FRIEND_SUCCESS = "ADD_FRIEND_SUCCESS";
+export const ADD_FRIEND_ERROR = "ADD_FRIEND_ERROR";
 
 export const fetchFriends = () => {
   return dispatch => {
@@ -13,6 +15,20 @@ export const fetchFriends = () => {
       })
       .catch(err => {
         dispatch({ type: FETCH_FRIENDS_ERROR, payload: err });
+      });
+  };
+};
+
+export const addFriends = friend => {
+  return dispatch => {
+    return axios
+      .post("http://localhost:5000/api/friends", friend)
+      .then(({ data }) => {
+        console.log("data is: ", data);
+        dispatch({ type: FETCH_FRIENDS_SUCCESS, payload: data });
+      })
+      .catch(err => {
+        dispatch({ type: ADD_FRIEND_ERROR, payload: err });
       });
   };
 };
